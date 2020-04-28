0e35579dbd3de7b83f8ba3bc95bdc6171b3e2097
00294961 <63437649+00294961@users.noreply.github.com>
2020-04-28 13:19:48 -0400

    Update project.py

--- project.py
+++ project.py
@@ -6,6 +6,11 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+
+# scoreboard
+score_1 = 0
+score_2 = 0
+
 # paddle1
 paddle_1 = turtle.Turtle()
 paddle_1.speed(0)
@@ -28,11 +33,22 @@ paddle_2.goto(350, 0)
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
-ball.color("white")
+ball.color("green")
 ball.penup()
 ball.goto(0, 0)
 ball.dx = 2
-ball.dy = 2
+ball.dy = -2
+
+# penscore 
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("red")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player 1: 0  Player 2: 0", align="center" , font=("Courier", 23, "normal"))
+
+
 # paddles movement 
 def paddle_1_up():
 	y = paddle_1.ycor()
@@ -66,3 +82,40 @@ while True:
 	
 	ball.setx(ball.xcor() + ball.dx)
 	ball.sety(ball.ycor() + ball.dy)
+
+
+
+# bouncing the ball
+	if ball.ycor() > 290:
+		ball.sety(290)
+		ball.dy *= -1
+	
+	if ball.ycor() < -290:
+		ball.sety(-290)
+		ball.dy *= -1
+	
+	if ball.xcor() > 390:
+		ball.goto(0,0)
+		ball.dx *= -1
+		score_1 += 1
+		pen.clear()
+		pen.write("Player 1: {}  Player 2: {}".format(score_1, score_2), align="center" , font=("Courier", 23, "normal"))
+	
+	if ball.xcor() < -390:
+		ball.goto(0,0)
+		ball.dx *= -1
+		score_2 += 1
+		pen.clear()
+		pen.write("Player 1: {}  Player 2: {}".format(score_1, score_2), align="center" , font=("Courier", 23, "normal"))
+		
+# bouncing the ball off the paddles
+	if ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < paddle_2.ycor() + 40 and ball.ycor() > paddle_2.ycor() - 40):
+		ball.setx(340)
+		ball.dx *= -1
+	
+	if ball.xcor() < -340 and ball.xcor() > -350 and (ball.ycor() < paddle_1.ycor() + 40 and ball.ycor() > paddle_1.ycor() - 40):
+		ball.setx(-340)
+		ball.dx *= -1
+
+
+
